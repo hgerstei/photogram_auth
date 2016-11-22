69daf3fd49605e412c8d11d72af945cbd7a46a62
Hilary Gerstein <hilary.gerstein@users.noreply.github.com>
2016-11-22 09:05:58 -0500

    Updated associations
    Some associations missing; likely cause of rake db:seed error

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,7 @@ class User < ActiveRecord::Base
   validates :username, :presence => true, :uniqueness => { :scope => :user_id }
 
   has_many :photos
+  has_many :comments
+  has_many :likes
   has_many :liked_photos, :through => :likes, :source => :photo
 end
